c53d634176a25e22cd01d1e29f24cf8c97c93e70
SparkleDai <379221797@qq.com>
2013-03-28 15:17:32 +0800

    LocalStorage

---
+++ LocalStorage/LocalStorage/JS/StorageTempSp.js
@@ -0,0 +1,14 @@
+﻿var StorageTempSp = {};
+
+StorageTempSp.StorageKeySet = {
+    Count: "Count",
+    Ascii: "Ascii"
+};
+
+StorageTempSp.ListenKeyValueChange = function (listenEvent) {//在调用setItem()、removeItem()或者clear()导致存储发生改变的时候会触发storage事件，但是如果你设置一个已存在的值，或者在一个空的存储区域调用clear()函数，就不会触发storage事件，这个事件不会冒泡也不可以被阻止。据测试，在改变的页面监听storage事件，有可能会无法监听到。需要在其他页面(如果是同一个页面，需要再打开一页监听)进行监听。
+    if (window.addEventListener) {
+        window.addEventListener("storage", listenEvent, false);
+    } else if (window.attachEvent) {
+        window.attachEvent("onstorage", listenEvent);
+    }
+}
